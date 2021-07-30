528a7894b6bfb642e275cd7f81c062ddd91ea17a
Pani Ania <mail@asondej.com>
2021-07-30 14:33:02 +0200

    moving everything except recipes to "app" folder

---
+++ yum/app/config.php
@@ -0,0 +1,7 @@
+<?php 
+declare(strict_types=1);
+
+const ABS_PATH = __DIR__;
+//const RECIPE_PATH = ;
+
+//dump(RECIPE_PATH );
--- yum/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-declare(strict_types=1);
-
-const ABS_PATH = __DIR__;
-//const TEMPLATES = 
-
-//dump($_SERVER);
